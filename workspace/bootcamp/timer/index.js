06d0a91ad659b59cc935e6cdb138185a26988414
Koichi <koichi89592@gmail.com>
2020-08-13 14:32:41 +0900

    Make timer project set

---
+++ workspace/bootcamp/timer/index.js
@@ -0,0 +1,23 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener(`click`, this.start);
+    }
+
+    start() {
+        console.log(`Timer to start the timer!`)
+    }
+    pause() {}
+    onDurationChange() {}
+    tick() {}
+}
+
+
+const durationInput = document.querySelector(`#duration`);
+const startButton = document.querySelector(`#start`);
+const pauseButton = document.querySelector(`#pause`);
+
+const timer = new Timer(durationInput, startButton, pauseButton);
