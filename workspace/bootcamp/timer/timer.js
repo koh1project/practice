69f634a6047350b6c81a8a0a993bb411e481fcbf
Koichi <koichi89592@gmail.com>
2020-08-14 13:42:54 +0900

    Modify timer function

--- workspace/bootcamp/timer/timer.js
+++ workspace/bootcamp/timer/timer.js
@@ -17,10 +17,10 @@ class Timer {
 
     start = () => {
         if (this.onStart) {
-            this.onStart();
+            this.onStart(this.timeRemaining);
         }
         this.tick();
-        this.interval = setInterval(this.tick, 1000);
+        this.interval = setInterval(this.tick, 50);
     }
     pause = () => {
         clearInterval(this.interval);
@@ -33,9 +33,9 @@ class Timer {
                 this.onComplete();
             }
         } else {
-            this.timeRemaining = this.timeRemaining - 1;
+            this.timeRemaining = this.timeRemaining - 0.05;
             if (this.onTick) {
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     };
@@ -44,7 +44,7 @@ class Timer {
         return Number.parseFloat(this.durationInput.value);
     }
     set timeRemaining(time) {
-        this.durationInput.value = time;
+        this.durationInput.value = time.toFixed(2);
     }
     onDurationChange = () => {};
 }
