e61ffec8aaade36317ff09dbe31e9b9ec3e4ec4d
Koichi <koichi89592@gmail.com>
2020-08-17 13:59:53 +0900

    Move menu from html to js

--- workspace/bootcamp/movies/index.js
+++ workspace/bootcamp/movies/index.js
@@ -6,28 +6,51 @@ const fetchData = async (searchTerm) => {
         }
     });
 
-    if(response.data.Error){
+    if (response.data.Error) {
         return [];
     }
-    
+
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results">
+        </div>
+    </div>
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+    resultsWrapper.innerHTML = '';
 
+    dropdown.classList.add('is-active');
     movies.forEach(movie => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
         `;
-        document.querySelector(('#target')).appendChild((div));
+
+        resultsWrapper.appendChild((option));
     });
-
-
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
