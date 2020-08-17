2be5e8f9bb2e19f9faf848b711ae7d1263671807
Koichi <koichi89592@gmail.com>
2020-08-17 10:48:44 +0900

    Set debounce

--- workspace/bootcamp/movies/index.js
+++ workspace/bootcamp/movies/index.js
@@ -1,11 +1,17 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com', {
         params: {
-            apikey: '',
-            i: 'tt0848228'
+            apikey: 'a476f990',
+            s: searchTerm
         }
     });
     console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+const onInput = event => {
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
