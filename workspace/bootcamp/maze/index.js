03c7cf9f125689f711b8ffa946a98e9d76ec8cd2
Koichi <koichi89592@gmail.com>
2020-08-18 21:34:28 +0900

    Add circle objects

--- workspace/bootcamp/maze/index.js
+++ workspace/bootcamp/maze/index.js
@@ -1,4 +1,7 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -7,12 +10,17 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: false,
+        width,
+        height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
 
 // Walls
 const walls = [
@@ -22,5 +30,28 @@ const walls = [
     Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ]
 World.add(world, walls);
+
+
+// Random Shapes
+
+
+for (let i = 0; i < 20; i++) {
+    if (Math.random() > 0.5) {
+        World.add(world, Bodies.rectangle(
+            Math.random() * width,
+            Math.random() * height,
+            50, 50));
+    } else {
+        World.add(world, Bodies.circle(
+            Math.random() * width,
+            Math.random() * height,
+            35, {
+                render: {
+                    fillStyle: 'red'
+                }
+            }
+        ));
+    }
+}
 
 World.add(world, Bodies.rectangle(200, 200, 50, 50));
