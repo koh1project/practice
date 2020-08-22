41d6f08b0a307d54d2b2f47a6578741760ef9376
Koichi <koichi89592@gmail.com>
2020-08-22 20:50:24 +0900

    Make constant variable for the number of cells

--- workspace/bootcamp/maze/index.js
+++ workspace/bootcamp/maze/index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -30,16 +31,14 @@ World.add(world, walls);
 
 //Maze generation
 
-const grid = Array(3)
+const grid = Array(cells)
     .fill(null)
-    .map(() => Array(3).fill(false));
+    .map(() => Array(cells).fill(false));
 
-const verticals = Array(3)
+const verticals = Array(cells)
     .fill(null)
-    .map(() => Array(2).fill(false));
+    .map(() => Array(cells - 1).fill(false));
 
-const horizontals = Array(2)
+const horizontals = Array(cells - 1)
     .fill(null)
-    .map(() => Array(3).fill(false));
-
-console.log(verticals);
+    .map(() => Array(cells).fill(false));
