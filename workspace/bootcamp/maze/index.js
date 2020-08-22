494b5116f1bb4b1139f398b3894de008c915d461
Koichi <koichi89592@gmail.com>
2020-08-22 21:35:59 +0900

    Set horizontal movement

--- workspace/bootcamp/maze/index.js
+++ workspace/bootcamp/maze/index.js
@@ -70,22 +70,38 @@ const stepThroughCell = (row, column) => {
 
     // Assemble randomly-order list of neighbors
     const neighbors = shuffle([
-        [row - 1, column], //above
-        [row, column + 1], //right
-        [row + 1, column], //below
-        [row, column - 1] //left
+        // [row - 1, column, 'up'],
+        [row, column + 1, 'right'],
+        // [row + 1, column, 'down'],
+        // [row, column - 1, 'left']
     ]);
 
     // For each neighbor...
+    for (let neighbor of neighbors) {
+        const [nextRow, nextColumn, direction] = neighbor;
 
-    // See if that neighbor is out of bounds
+        // See if that neighbor is out of bounds
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue;
+        }
 
-    // If we have visited that neighbor, continue to next neighbor
+        // If we have visited that neighbor, continue to next neighbor
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        }
 
-    // Remove a wall from either horizontals or verticals
+        // Remove a wall from either horizontals or verticals
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+        }
+        if (direction === 'right') {
+            verticals[row][column] = true;
+        }
 
+    }
     // Visit that next cell
 
 };
 
-stepThroughCell(startRow, startColumn);
+// stepThroughCell(startRow, startColumn);
+stepThroughCell(1, 1);
