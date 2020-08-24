f4b47eda65c2eebb91ffd59475d97de7b2a272b6
Koichi <koichi89592@gmail.com>
2020-08-24 19:05:39 +0900

    Make wining motion

--- workspace/bootcamp/maze/index.js
+++ workspace/bootcamp/maze/index.js
@@ -123,6 +123,7 @@ horizontals.forEach((row, rowIndex) => {
             ((rowIndex * unitLength) + unitLength),
             unitLength,
             5, {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -140,6 +141,7 @@ verticals.forEach((row, rowIndex) => {
             ((rowIndex * unitLength) + (unitLength / 2)),
             5,
             unitLength, {
+                label: 'wall',
                 isStatic: true
             }
         );
@@ -197,7 +199,12 @@ Events.on(engine, 'collisionStart', event => {
 
         if (labels.includes(collision.bodyA.label) &&
             labels.includes(collision.bodyB.label)) {
-                console.log('User won');
+            world.gravity.y = 1;
+            world.bodies.forEach(body => {
+                if (body.label === 'wall') {
+                    Body.setStatic(body, false);
+                }
+            })
         }
     })
 });
