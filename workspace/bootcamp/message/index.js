665ec67c49f7ea950db0e9759c0e94baa20a97ed
Koichi <koichi89592@gmail.com>
2020-08-25 19:46:31 +0900

    Make url generator

--- workspace/bootcamp/message/index.js
+++ workspace/bootcamp/message/index.js
@@ -1,9 +1,9 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
     const input = document.querySelector('#message-input');
-
     const encrypted = btoa(input.value);
-    console.log(encrypted);
 
-    document.querySelector('#link-input').value = encrypted;
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 });
