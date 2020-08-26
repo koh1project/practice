d58a2d2137ab57f619c0ce4ea00bb6f9bb4bb9ec
Koichi <koichi89592@gmail.com>
2020-08-26 19:11:59 +0900

    Substitute proecess.cwd for directory

--- workspace/bootcamp/list/index.js
+++ workspace/bootcamp/list/index.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 
-fs.readdir('.', (err, filenames) => {
+fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.error(err);
     }
-
     console.log(filenames);
 });
