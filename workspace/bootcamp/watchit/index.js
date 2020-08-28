a08a1ac672ebb969ea0348353a2f789f3994542f
Koichi <koichi89592@gmail.com>
2020-08-28 16:43:29 +0900

    Use lodash.debounce

--- workspace/bootcamp/watchit/index.js
+++ workspace/bootcamp/watchit/index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 
+const start = debounce(() => {
+    console.log('STARTING USER PROGRAM');
+}, 100);
+
 chokidar.watch('.')
-    .on('add', () =>console.log('FILE ADDED'))
-    .on('change', () =>console.log('FILE CHANGED'))
-    .on('unlink', () =>console.log('FILE UNLINKED'));
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log('FILE UNLINKED'));
