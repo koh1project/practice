bb29410ae8b0f44711251aa54d848f511ac84d2f
Koichi <koichi89592@gmail.com>
2020-08-28 14:52:04 +0900

    Change so it can accept command arguments

--- workspace/bootcamp/list/index.js
+++ workspace/bootcamp/list/index.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 const util = require('util');
+const chalk = require('chalk');
+const path = require('path');
 
 // Method #2
 // const lstat = util.promisify(fs.lstat);
@@ -9,20 +11,26 @@ const util = require('util');
 // Method #3
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         console.error(err);
     }
 
     const statPromises = filenames.map(filename => {
-        return lstat(filename);
+        return lstat(path.join(targetDir, filename));
     });
 
     const allStats = await Promise.all(statPromises);
 
     for (let stats of allStats) {
         const index = allStats.indexOf(stats);
-        console.log(filenames[index], stats.isFile());
+        if(stats.isFile()){
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.bold(filenames[index]));
+        }
     }
 });
 // Method #1
