f816d0d5672793164413b64d7ebc5065512265d7
Koichi <koichi89592@gmail.com>
2020-08-28 13:12:14 +0900

    Make bad code

--- workspace/bootcamp/list/index.js
+++ workspace/bootcamp/list/index.js
@@ -1,8 +1,18 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
     if (err) {
         console.error(err);
     }
-    console.log(filenames);
+
+    // BAD CODE
+    for (let filename of filenames) {
+        fs.lstat(filename, (err, stats) => {
+            if (err) { console.log(err) }
+
+            console.log(filename,stats.isFile());
+        });
+    }
 });
