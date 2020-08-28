50f870f047d9d55e73acc65499e1ff3294d0813e
Koichi <koichi89592@gmail.com>
2020-08-28 14:04:21 +0900

    Refactor : use await Promise.All

--- workspace/bootcamp/list/index.js
+++ workspace/bootcamp/list/index.js
@@ -13,17 +13,18 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.error(err);
     }
-    for (let filename of filenames) {
-        try {
-            const stats = await lstat(filename);
 
-            console.log(filename, stats.isFile());
-        } catch (err) {
-            console.log(err);
-        }
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+        console.log(filenames[index], stats.isFile());
     }
 });
-
 // Method #1
 // const lstat = (filename) => {
 //     return new Promise((resolve, reject) => {
