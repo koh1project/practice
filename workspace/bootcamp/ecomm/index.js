0394b6664354e9bf241853879a7022f0a883050a
Koichi <koichi89592@gmail.com>
2020-08-29 22:02:15 +0900

    Make a response

--- workspace/bootcamp/ecomm/index.js
+++ workspace/bootcamp/ecomm/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    
+    res.send('Hi there!');
+});
+
+app.listen(3000, ()=> {
+    console.log('Listening');
+});
