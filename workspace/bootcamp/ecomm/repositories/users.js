8af3e71dc2f526dc23a9bc48f2160d1f67bad001
Koichi <koichi89592@gmail.com>
2020-08-31 23:36:24 +0900

    Make update function

--- workspace/bootcamp/ecomm/repositories/users.js
+++ workspace/bootcamp/ecomm/repositories/users.js
@@ -45,14 +45,25 @@ class UsersRepository {
         const records = await this.getAll();
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
+    }
 
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    // await repo.delete('0eb30fb4');
+    await repo.update('aaa', {password:'mypassword'});
 };
 
 test();
