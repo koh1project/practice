c105da4c22c0ccd6179f6731bc31e7fbd22439ac
Koichi <koichi89592@gmail.com>
2020-09-06 21:49:03 +0900

    Move validator functions to validators.js

--- workspace/bootcamp/ecomm/routes/admin/auth.js
+++ workspace/bootcamp/ecomm/routes/admin/auth.js
@@ -4,28 +4,15 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', [
-    check('email').trim().normalizeEmail().isEmail()
-    .custom(async email => {
-        const existingUser = await usersRepo.getOneBy({ email });
-        if (existingUser) {
-            throw new Error('Email in use');
-        }
-    }),
-    check('password').trim().isLength({ min: 4, max: 20 }),
-    check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
-    .custom(async (passwordConfirmation, { req }) => {
-        if (passwordConfirmation !== req.body.password) {
-            throw new Error('Passwords must match');
-        }
-    })
-], async (req, res) => {
+router.post('/signup', [requireEmail, requirePassword, requirePasswordConfirmation], async (req, res) => {
     const errors = validationResult(req);
     console.error(errors);
 
