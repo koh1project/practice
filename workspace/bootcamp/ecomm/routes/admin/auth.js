42292ef50ec75baf1c6bac7b973c257352b32f35
Koichi <koichi89592@gmail.com>
2020-09-06 21:38:31 +0900

    Make custom validations

--- workspace/bootcamp/ecomm/routes/admin/auth.js
+++ workspace/bootcamp/ecomm/routes/admin/auth.js
@@ -11,28 +11,27 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', [
-    check('email').trim().normalizeEmail().isEmail(),
+    check('email').trim().normalizeEmail().isEmail()
+    .custom(async email => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+            throw new Error('Email in use');
+        }
+    }),
     check('password').trim().isLength({ min: 4, max: 20 }),
     check('passwordConfirmation').trim().isLength({ min: 4, max: 20 })
+    .custom(async (passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+            throw new Error('Passwords must match');
+        }
+    })
 ], async (req, res) => {
     const errors = validationResult(req);
     console.error(errors);
 
     const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await usersRepo.getOneBy({ email });
-
-    if (existingUser) {
-        return res.send('Email in use');
-    }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match');
-    }
-
-    // Create a user in our user repo to represent this person
     const user = await usersRepo.create(({ email, password }));
 
-    // Store the id of that user inside the users cookie
     req.session.userId = user.id;
 
     res.send(req.body);
