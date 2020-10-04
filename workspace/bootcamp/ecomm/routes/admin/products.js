ec178f88b89ca83d869034843fc9cd58b3f3da46
Koichi <koichi89592@gmail.com>
2020-10-04 18:51:26 +0900

    Set Authentication

--- workspace/bootcamp/ecomm/routes/admin/products.js
+++ workspace/bootcamp/ecomm/routes/admin/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 const multer = require('multer');
 
-const { handleErrors } = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
@@ -13,6 +13,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.get(
     '/admin/products',
+    requireAuth,
     async (req, res) => {
         const products = await productsRepo.getAll();
         res.send(productsIndexTemplate({ products }));
@@ -20,16 +21,19 @@ router.get(
 
 router.get(
     '/admin/products/new',
+    requireAuth,
     (req, res) => {
         res.send(productsNewTemplate({}));
     });
 
 router.post(
     '/admin/products/new',
+    requireAuth,
     upload.single('image'),
     [requireTitle, requirePrice],
     handleErrors(productsNewTemplate),
     async (req, res) => {
+
         const image = req.file.buffer.toString('base64');
         const { title, price } = req.body;
         await productsRepo.create({ title, price, image });
