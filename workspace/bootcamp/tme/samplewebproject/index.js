4dbb5d9c29928ffbed60c2cacea470dd3ef58d07
Koichi <koichi89592@gmail.com>
2020-10-15 16:40:35 +0900

    Add fail message test

--- workspace/bootcamp/tme/samplewebproject/index.js
+++ workspace/bootcamp/tme/samplewebproject/index.js
@@ -4,7 +4,7 @@ document.querySelector('form').addEventListener('submit', (event) => {
     const { value } = document.querySelector('input');
     const header = document.querySelector('h1');
     if (value.includes('@')) {
-        header.innerHTML = 'Looks good';
+        header.innerHTML = 'Looks good!';
     } else {
         header.innerHTML = 'Invalid email';
     }
