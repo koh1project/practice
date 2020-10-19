8fc7550db1d5e56c1783957987b907aaca10b0e3
Koichi <koichi89592@gmail.com>
2020-10-19 22:35:07 +0900

    Use ts-loader

--- workspace/foodapp/webpack.config.js
+++ workspace/foodapp/webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/main.js',
+  entry: './src/food-app/main.ts',
   output: {
     // filename: '[contenthash]bundle.js',
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map'
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
 }
