624dc6adfa06539cdcc25ebc19c26446bce9cd49
Koichi <koichi89592@gmail.com>
2020-10-19 15:36:34 +0900

    Add Eventlistener

--- workspace/foodapp/src/food-app.ts
+++ workspace/foodapp/src/food-app.ts
@@ -1,3 +1,18 @@
-class Score { }
-class Food { }
-class Foods { }
+class Score {}
+class Food {
+  constructor(public element: HTMLDivElement) {
+    element.addEventListener('click', this.clickEventHandler);
+  }
+  clickEventHandler() {
+    this.element.classList.toggle('food-active');
+  };
+}
+class Foods {
+  elements = document.querySelectorAll < HTMLDivElement > ('.food');
+  constructor() {
+    this.elements.forEach(element => {
+      new Food(element);
+    })
+  }
+}
+const foods = new Foods();
