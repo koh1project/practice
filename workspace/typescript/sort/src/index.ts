ccef43cf0ec60c3440db25969d4cae3d560ecf36
Koichi <koichi89592@gmail.com>
2020-10-26 17:27:10 +0900

    Make class for string sorting

--- workspace/typescript/sort/src/index.ts
+++ workspace/typescript/sort/src/index.ts
@@ -1,8 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0, 50]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(sorter.collection.data);
+
+const charactersCollection  = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection.data);
 
