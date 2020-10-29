392ef36db862deea5294af260e6ec254c1eb75f0
Koichi <koichi89592@gmail.com>
2020-10-29 15:47:39 +0900

    Extract mapRow method

--- workspace/typescript/stats/src/CsvFileReader.ts
+++ workspace/typescript/stats/src/CsvFileReader.ts
@@ -18,16 +18,18 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          Number.parseInt(row[3]),
-          Number.parseInt(row[4]),
-          row[5] as MatchResult, // "H", "A", "D"
-          row[6]
-        ];
-      });
+      .map(this.mapRow);
+  }
+  
+  mapRow(row: string[]): MatchData {
+    return [
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      Number.parseInt(row[3]),
+      Number.parseInt(row[4]),
+      row[5] as MatchResult, // "H", "A", "D"
+      row[6]
+    ];
   }
 }
