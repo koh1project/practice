58d243a20f27c31ed0381aa6e6f492f865aa165d
Koichi <koichi89592@gmail.com>
2020-10-30 18:50:14 +0900

    Extract User related functions to other classes

--- workspace/typescript/web/src/models/User.ts
+++ workspace/typescript/web/src/models/User.ts
@@ -1,15 +1,13 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
   get(propName: string): number | string {
@@ -17,37 +15,5 @@ export class User {
   }
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || []; // Callback or undefined
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
-  fetch(): void {
-    const id = this.get('id');
-    axios.get(`http://localhost:3000/users/${id}`).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (this.get('id')) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users/', this.data);
-    }
   }
 }
