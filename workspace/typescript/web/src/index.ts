f260c168c614449021533aa5215dfa8f0f453852
Koichi <koichi89592@gmail.com>
2020-10-30 13:13:42 +0900

    Change User field optional

--- workspace/typescript/web/src/index.ts
+++ workspace/typescript/web/src/index.ts
@@ -5,7 +5,8 @@ const user = new User({ name: 'myName', age: 20 });
 console.log(user.get('name'));
 console.log(user.get('age'));
 
-user.set({ name: 'newName', age: 999 });
+// user.set({ name: 'newName', age: 999 });
+user.set({ name: 'newName' });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
