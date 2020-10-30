bbca109ec6a1713447fb1aab1429a80ff6355bc8
Koichi <koichi89592@gmail.com>
2020-10-30 11:13:48 +0900

    Change index.ts

--- workspace/typescript/stats/src/index.ts
+++ workspace/typescript/stats/src/index.ts
@@ -1,24 +1,17 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WindsAnalysis";
+import { Summary } from "./Summary";
 
-// Create an object that satisfies the 'dataReader' interface
 const csvFileReader = new CsvFileReader("football.csv");
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-// matchReader.matches
 
-let manUnitedWins = 0;
+const summary = new Summary(
+  new WinsAnalysis("Man United"),
+  new ConsoleReport()
+);
 
-for (let match of matchReader.matches) {
-  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
