070ffa4508bae87a0bf5a4b5a07876da62b09420
Koichi <koichi89592@gmail.com>
2020-10-30 12:29:19 +0900

    Add print functions with generics

--- workspace/typescript/features/generics.ts
+++ workspace/typescript/features/generics.ts
@@ -22,3 +22,25 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < ArrayOfAnything.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < ArrayOfAnything.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < ArrayOfAnything.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+// ==
+printAnything(['a', 'b', 'c']);
