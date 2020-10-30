689a04f8e5e185c3863c397316de6cd1de2198a5
Koichi <koichi89592@gmail.com>
2020-10-30 12:44:15 +0900

    Add functions

--- workspace/typescript/features/generics.ts
+++ workspace/typescript/features/generics.ts
@@ -44,3 +44,31 @@ function printAnything<T>(arr: T[]): void {
 printAnything<string>(['a', 'b', 'c']);
 // ==
 printAnything(['a', 'b', 'c']);
+
+// Generic Constraints
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars([new Car()]);
+printHousesOrCars([new House(), new Car()]);
+
