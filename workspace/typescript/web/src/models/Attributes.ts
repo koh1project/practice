af1e2e2467e1b980556609c00411dd0085dbbb4b
Koichi <koichi89592@gmail.com>
2020-10-30 19:45:36 +0900

    Remove testing code

--- workspace/typescript/web/src/models/Attributes.ts
+++ workspace/typescript/web/src/models/Attributes.ts
@@ -1,5 +1,3 @@
-import { UserProps } from './User';
-
 export class Attributes<T> {
   constructor(private data: T) {}
   get<K extends keyof T>(key: K): T[K] {
@@ -9,13 +7,3 @@ export class Attributes<T> {
     Object.assign(this.data, update);
   }
 }
-
-const attrs = new Attributes<UserProps>({
-  id: 5,
-  age: 20,
-  name: 'Whatever',
-});
-
-const name = attrs.get('name'); // string
-const age = attrs.get('age'); // number
-const id = attrs.get('id'); // number
