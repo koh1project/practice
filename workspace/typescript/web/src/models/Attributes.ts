dd7c07b3f2e0894dcd3cbc682c00860080c93d5c
Koichi <koichi89592@gmail.com>
2020-10-30 19:45:13 +0900

    Use generics constraint in Attributes.ts

--- workspace/typescript/web/src/models/Attributes.ts
+++ workspace/typescript/web/src/models/Attributes.ts
@@ -1,9 +1,21 @@
+import { UserProps } from './User';
+
 export class Attributes<T> {
   constructor(private data: T) {}
-  get(propName: string): number | string | boolean {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
   set(update: T): void {
     Object.assign(this.data, update);
   }
 }
+
+const attrs = new Attributes<UserProps>({
+  id: 5,
+  age: 20,
+  name: 'Whatever',
+});
+
+const name = attrs.get('name'); // string
+const age = attrs.get('age'); // number
+const id = attrs.get('id'); // number
