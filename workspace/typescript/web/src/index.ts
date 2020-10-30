c974c6820d65bda8ece93ac6244345a68d95e398
Koichi <koichi89592@gmail.com>
2020-10-30 17:35:37 +0900

    Add save function

--- workspace/typescript/web/src/index.ts
+++ workspace/typescript/web/src/index.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
+import { User } from './models/User';
 
-axios.post('http://localhost:3000/users', {
-  name: 'myName',
-  age: 20,
-});
+const user = new User({ name: 'new record', age: 0 });
 
-const id = axios.get('http://localhost:3000/users/1');
+// user.save();
--- workspace/typescript/web/src/models/User.ts
+++ workspace/typescript/web/src/models/User.ts
@@ -36,10 +36,18 @@ export class User {
     });
   }
   fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
+    const id = this.get('id');
+    axios.get(`http://localhost:3000/users/${id}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const id = this.get('id');
+    if (this.get('id')) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      axios.post('http://localhost:3000/users/', this.data);
+    }
   }
 }
