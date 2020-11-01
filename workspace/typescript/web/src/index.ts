79de66f0b184916130babfc5c09c99065a655ee7
Koichi <koichi89592@gmail.com>
2020-11-01 09:24:59 +0900

    Set getters

--- workspace/typescript/web/src/index.ts
+++ workspace/typescript/web/src/index.ts
@@ -3,8 +3,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'new record', age: 0 });
 
-user.events.on('change', () => {
-  console.log('change #1');
+user.on('change', () => {
+  console.log('user was changed');
 });
-
-user.events.trigger('change');
+user.trigger('change');
