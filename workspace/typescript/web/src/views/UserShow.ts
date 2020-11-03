b49d9f6d86fe7b6a827c8531914b7358cc50ae2e
Koichi <koichi89592@gmail.com>
2020-11-03 17:37:08 +0900

    Implement insert region map inserting

--- workspace/typescript/web/src/views/UserShow.ts
+++ workspace/typescript/web/src/views/UserShow.ts
@@ -5,7 +5,7 @@ export class UserShow extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1><User Detail/h1>
+        <h1>User Detail</h1>
         <div>User Name: ${this.model.get('name')}</div>
         <div>User Name: ${this.model.get('age')}</div>
       </div>
