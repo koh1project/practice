8b111fcd0529d3a06162baa6448099ea1dd52cfc
Koichi <koichi89592@gmail.com>
2020-11-05 18:43:08 +0900

    Add a variable in Person component

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
         < Person />
+        < Person />
+        < Person />
       </div>
     );
 
