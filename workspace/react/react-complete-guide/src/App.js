01284d4aff5e486694c3ca863c2b34aa7cf75298
Koichi <koichi89592@gmail.com>
2020-11-05 18:41:39 +0900

    Add person component

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is working</p>
+        < Person />
       </div>
     );
+
+    // return React.createElement(('div'));
   }
 }
+
 
 export default App;
