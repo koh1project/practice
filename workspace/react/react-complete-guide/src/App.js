26af183649fdf6fb23bed174024300d452996f66
Koichi <koichi89592@gmail.com>
2020-11-06 17:18:35 +0900

    Change bind to arrow function, but it inefficient

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
-        <button onClick={ this.switchNameHandler.bind(this, 'Another') }>Switch Name</button>
+        <button onClick={ () => this.switchNameHandler('In arrow') }>Switch Name</button>
         < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }
         click={this.switchNameHandler.bind(this, 'In render')} />
         < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
