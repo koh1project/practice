014af230a88f984735671e71a1b26e92617b3bf8
Koichi <koichi89592@gmail.com>
2020-11-06 16:01:53 +0900

    Add setState

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -11,12 +11,20 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    // console.log('Was Clicked!');
+    this.setState({persons:[
+      {name: 'Altered', age: 20},
+      {name: 'Mike', age: 18},
+      {name: 'Sam', age: 100}
+    ]});
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         < Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         < Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         < Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
