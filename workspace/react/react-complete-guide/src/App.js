049781855bb9ce54e58e3399460285b687fa145e
Koichi <koichi89592@gmail.com>
2020-11-06 17:04:26 +0900

    Revert "Change for React hooks, functional component"
    This reverts commit dce2e2e91857f147f8889a11211bd2e6de38a5fd.

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -1,43 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const[ personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 20 },
       { name: 'Mike', age: 18 },
       { name: 'Sam', age: 0 }
-    ],
-    otherState: 'Some other values'
-  });
+    ]
+  }
 
-  console.log(personsState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons:[
-        { name: 'Altered', age: 20 },
-        { name: 'Mike', age: 18 },
-        { name: 'Sam', age: 100 }
-    ],
-    otherState: personsState.otherState
-  });
-  };
-
+  switchNameHandler = () => {
+    // console.log('Was Clicked!');
+    this.setState({persons:[
+      { name: 'Altered', age: 20 },
+      { name: 'Mike', age: 18 },
+      { name: 'Sam', age: 100 }
+    ]});
+  }
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
-        <button onClick={ switchNameHandler }>Switch Name</button>
-        < Person name={ personsState.persons[0].name } age={ personsState.persons[0].age } />
-        < Person name={ personsState.persons[1].name } age={ personsState.persons[1].age } />
-        < Person name={ personsState.persons[2].name } age={ personsState.persons[2].age } />
+        <button onClick={ this.switchNameHandler }>Switch Name</button>
+        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
+        < Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
     );
 
     // return React.createElement(('div'));
+  }
 }
 
 
-export default app;
+export default App;
