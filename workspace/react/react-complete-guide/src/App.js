6292273621c1f9ea07896995ddae5d08686ef240
Koichi <koichi89592@gmail.com>
2020-11-06 17:31:22 +0900

    Add input and change handler

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -13,10 +13,21 @@ class App extends Component {
 
   switchNameHandler = (newName) => {
     // console.log('Was Clicked!');
-    this.setState({persons:[
-      { name: newName, age: 20 },
-      { name: 'Mike', age: 18 },
-      { name: 'Sam', age: 100 }
+    this.setState({
+      persons:[
+        { name: newName, age: 20 },
+        { name: 'Mike', age: 18 },
+        { name: 'Sam', age: 100 }
+    ]});
+  }
+
+  nameChangedHandler = (e)  => {
+    console.log(e.target.value)
+    this.setState({
+      persons:[
+        { name: 'Max', age: 20 },
+        { name: e.target.value, age: 18 },
+        { name: 'Sam', age: 100 }
     ]});
   }
   render() {
@@ -25,9 +36,10 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
         <button onClick={ () => this.switchNameHandler('In arrow') }>Switch Name</button>
-        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }
-        click={this.switchNameHandler.bind(this, 'In render')} />
-        < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
+        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }
+        click={this.switchNameHandler.bind(this, 'In render')}
+        changed={this.nameChangedHandler}/>
         < Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
     );
