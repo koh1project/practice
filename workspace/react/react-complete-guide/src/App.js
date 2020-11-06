dce2e2e91857f147f8889a11211bd2e6de38a5fd
Koichi <koichi89592@gmail.com>
2020-11-06 16:21:18 +0900

    Change for React hooks, functional component

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const[ personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 20 },
       { name: 'Mike', age: 18 },
       { name: 'Sam', age: 0 }
-    ]
-  }
+    ],
+    otherState: 'Some other values'
+  });
 
-  switchNameHandler = () => {
-    // console.log('Was Clicked!');
-    this.setState({persons:[
-      { name: 'Altered', age: 20 },
-      { name: 'Mike', age: 18 },
-      { name: 'Sam', age: 100 }
-    ]});
-  }
-  render() {
+  console.log(personsState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons:[
+        { name: 'Altered', age: 20 },
+        { name: 'Mike', age: 18 },
+        { name: 'Sam', age: 100 }
+    ],
+    otherState: personsState.otherState
+  });
+  };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
-        < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
-        < Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
+        <button onClick={ switchNameHandler }>Switch Name</button>
+        < Person name={ personsState.persons[0].name } age={ personsState.persons[0].age } />
+        < Person name={ personsState.persons[1].name } age={ personsState.persons[1].age } />
+        < Person name={ personsState.persons[2].name } age={ personsState.persons[2].age } />
       </div>
     );
 
     // return React.createElement(('div'));
-  }
 }
 
 
-export default App;
+export default app;
