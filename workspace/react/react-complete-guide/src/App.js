49075ef1b5c813de2b7bf183aa2fe5dd164d91f0
Koichi <koichi89592@gmail.com>
2020-11-06 17:10:28 +0900

    Use bind

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -11,10 +11,10 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was Clicked!');
     this.setState({persons:[
-      { name: 'Altered', age: 20 },
+      { name: newName, age: 20 },
       { name: 'Mike', age: 18 },
       { name: 'Sam', age: 100 }
     ]});
@@ -24,8 +24,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
-        <button onClick={ this.switchNameHandler }>Switch Name</button>
-        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        <button onClick={ this.switchNameHandler.bind(this, 'Another') }>Switch Name</button>
+        < Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }
+        click={this.switchNameHandler.bind(this, 'In render')} />
         < Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
         < Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
