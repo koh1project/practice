67d7ae6cd4541ad0e89ce59e881e52572da217be
Koichi <koichi89592@gmail.com>
2020-11-07 16:57:17 +0900

    Change way for checking state.showPersons

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -46,22 +46,27 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+          <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }
+          click={this.switchNameHandler.bind(this, 'In render')} changed={this.nameChangedHandler} />
+          <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working</p>
         <button
-        style={ style }
-        onClick={  this.togglePersonsHandler }>Toggle Persons</button>
-        {
-          this.state.showPersons === true ?
-            <div>
-              <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
-              <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }
-              click={this.switchNameHandler.bind(this, 'In render')} changed={this.nameChangedHandler} />
-              <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
-          </div> : null
-        }
+          style={ style }
+          onClick={  this.togglePersonsHandler }>Toggle Persons</button>
+        { persons }
       </div>
     );
 
