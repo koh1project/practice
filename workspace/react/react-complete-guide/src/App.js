8a9205d2267cf5945eab7bdb10dbad51fb06166e
Koichi <koichi89592@gmail.com>
2020-11-07 19:07:41 +0900

    Change button style with Styled-components

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -43,17 +59,7 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
+    const style = {};
 
     let persons = null;
 
@@ -90,9 +96,9 @@ class App extends Component {
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={ classes.join(' ') }>This is working</p>
-          <button
-            style={ style }
-            onClick={  this.togglePersonsHandler }>Toggle Persons</button>
+          <StyledButton
+            onClick={  this.togglePersonsHandler }>Toggle Persons
+          </StyledButton>
           { persons }
         </div>
     );
