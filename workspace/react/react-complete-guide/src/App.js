ba4a25ce91f9ae5a9bd782e6fd29fb5b2a1414d2
Koichi <koichi89592@gmail.com>
2020-11-07 18:17:39 +0900

    Change nameChangedHandler

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -6,9 +6,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { id:"sasdf", name: 'Max', age: 20 },
-      { id:"fda", name: 'Mike', age: 18 },
-      { id:"gdfa",name: 'Sam', age: 0 }
+      { id:"1", name: 'Max', age: 20 },
+      { id:"2", name: 'Mike', age: 18 },
+      { id:"3",name: 'Sam', age: 0 }
     ],
     otherState: 'Some other values',
     showPersons: false
@@ -21,13 +21,21 @@ class App extends Component {
     this.setState({ persons: persons });
   }
 
-  nameChangedHandler = (e)  => {
-    this.setState({
-      persons:[
-        { name: 'Max', age: 20 },
-        { name: e.target.value, age: 18 },
-        { name: 'Sam', age: 100 }
-    ]});
+  nameChangedHandler = (e, id)  => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    person.name = e.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -52,9 +60,10 @@ class App extends Component {
           { this.state.persons.map( (person, index) => {
             return <Person
               key={ person.id }
-              name={ person.name}
+              name={ person.name }
               age={ person.age }
-              click={ () => this.deletePersonHandler(index) } />
+              click={ () => this.deletePersonHandler(index) }
+              changed={ (event) => this.nameChangedHandler(event, person.id) } />
           })}
         </div>
       );
