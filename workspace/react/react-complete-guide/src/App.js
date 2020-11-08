218db88e1dc70b8d810509f473cbb0f5d8e4d52b
Koichi <koichi89592@gmail.com>
2020-11-08 18:55:33 +0900

    Make an error

--- workspace/react/react-complete-guide/src/App.js
+++ workspace/react/react-complete-guide/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   nameChangedHandler = (e, id)  => {
     const personIndex = this.state.persons.findIndex(p => {
       // return p.id === id;
-      return p.id === id;
+      return p.userId === id;
     });
 
     const person = {
