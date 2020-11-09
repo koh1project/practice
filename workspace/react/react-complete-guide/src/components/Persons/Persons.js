ae3d78c9ccbd4ccd88cb61fa7975b6a5e7d5405b
Koichi <koichi89592@gmail.com>
2020-11-09 16:19:31 +0900

    Convert Person.js to class based

--- workspace/react/react-complete-guide/src/components/Persons/Persons.js
+++ workspace/react/react-complete-guide/src/components/Persons/Persons.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  return props.persons.map((person, index) => {
+class Persons extends Component {
+  render() {
     console.log('[Persons.js] rendering');
-    return <Person
-      key={ person.id }
-      name={ person.name }
-      age={ person.age }
-      click={ () => props.clicked(index) }
-      changed={(event) => props.changed(event, person.id)}
-    />
-  })
-};
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={ person.id }
+        name={ person.name }
+        age={ person.age }
+        click={ () => this.props.clicked(index) }
+        changed={(event) => this.props.changed(event, person.id)}
+      />
+    })
+  }
+}
 
-
-
-export default persons;
+export default Persons;
