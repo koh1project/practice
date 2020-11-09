cba886e7d54610e654625cecd4dcfaf9c97d295c
Koichi <koichi89592@gmail.com>
2020-11-09 17:44:30 +0900

    Add life cycle methods in App.js

--- workspace/react/react-complete-guide/src/containers/App.js
+++ workspace/react/react-complete-guide/src/containers/App.js
@@ -33,6 +33,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount')
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[App.js] componentDidUpdate')
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
