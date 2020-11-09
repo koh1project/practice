4da8f78c235c23d966c22066da855a0ca70b8642
Koichi <koichi89592@gmail.com>
2020-11-09 16:17:55 +0900

    Convert Person.js to class based

--- workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
+++ workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-
+class Person extends Component {
+render() {
+  console.log('[Person.js] rendering');
   return(
     <div className={ classes.Person }>
-      <p onClick={props.click}>I 'm a {props.name}! I'm  {props.age}years old </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+      <p onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>
+      <p>{this.props.children}</p>
+      <input type="text" onChange={this.props.changed} value={this.props.name}/>
     </div>
-  )
-};
+    )
+  }
+}
 
-export default person;
+export default Person;
