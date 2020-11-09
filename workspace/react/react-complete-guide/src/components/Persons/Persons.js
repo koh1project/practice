9dc185be32682cd3955c44ae8b44c93f2620a7a3
Koichi <koichi89592@gmail.com>
2020-11-09 17:32:11 +0900

    Add lifecycle methods in Persons.js

--- workspace/react/react-complete-guide/src/components/Persons/Persons.js
+++ workspace/react/react-complete-guide/src/components/Persons/Persons.js
@@ -2,16 +2,47 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+
+  // @deprecated
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // @deprecated
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps');
+  // }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  // @deprecated
+  // componentWillUpdate(nextProps, nextState, nextContext) {}
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
   render() {
     console.log('[Persons.js] rendering');
     return this.props.persons.map((person, index) => {
-      return <Person
-        key={ person.id }
-        name={ person.name }
-        age={ person.age }
-        click={ () => this.props.clicked(index) }
-        changed={(event) => this.props.changed(event, person.id)}
-      />
+      return (
+        <Person
+          key={ person.id }
+          name={ person.name }
+          age={ person.age }
+          click={ () => this.props.clicked(index) }
+          changed={(event) => this.props.changed(event, person.id)}
+        />)
     })
   }
 }
