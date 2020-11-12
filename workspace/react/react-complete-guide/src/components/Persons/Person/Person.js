53a6f44b22fc49840c10bb288cfa8d00693e70c8
Koichi <koichi89592@gmail.com>
2020-11-12 15:51:36 +0900

    Use adjacent elements

--- workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
+++ workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
@@ -4,12 +4,13 @@ import classes from './Person.css';
 class Person extends Component {
 render() {
   console.log('[Person.js] rendering');
-  return(
-    <div className={classes.Person}>
-      <p onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>
-      <p>{this.props.children}</p>
-      <input type="text" onChange={this.props.changed} value={this.props.name}/>
-    </div>
+  return([
+    // <div className={classes.Person}>
+      <p key="i1" onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>,
+      <p key="i2">{this.props.children}</p>,
+      <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
+      ]
+    // </div>
     )
   }
 }
