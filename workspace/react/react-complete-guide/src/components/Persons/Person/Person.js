8c7e56845a6f3ad66b969f0181e2624dfd9e7e41
Koichi <koichi89592@gmail.com>
2020-11-12 16:07:30 +0900

    Reset Person.js

--- workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
+++ workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary'
 import classes from './Person.css';
@@ -7,11 +7,11 @@ class Person extends Component {
 render() {
   console.log('[Person.js] rendering');
   return(
-    <Fragment>
+    <Aux>
       <p key="i1" onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>
       <p key="i2">{this.props.children}</p>
       <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-    </Fragment>
+    </Aux>
     )
   }
 }
