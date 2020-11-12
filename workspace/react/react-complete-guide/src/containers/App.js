257d2142049f63e24e72c6765d4c140807c9b1f3
Koichi <koichi89592@gmail.com>
2020-11-12 16:36:36 +0900

    Add bad state change

--- workspace/react/react-complete-guide/src/containers/App.js
+++ workspace/react/react-complete-guide/src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -61,7 +62,7 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState({persons: persons, changeCounter: this.state.changeCounter + 1});
   };
 
   deletePersonHandler = personIndex => {
