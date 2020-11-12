ad4c448594e0c570020ede055ea4eef08852ff96
Koichi <koichi89592@gmail.com>
2020-11-12 15:58:22 +0900

    Make Auxiliary.js

--- workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
+++ workspace/react/react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+
+import Aux from '../../../hoc/Auxiliary'
 import classes from './Person.css';
 
 class Person extends Component {
 render() {
   console.log('[Person.js] rendering');
-  return([
-    // <div className={classes.Person}>
-      <p key="i1" onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>,
-      <p key="i2">{this.props.children}</p>,
+  return(
+    <Aux>
+      <p key="i1" onClick={this.props.click}>I 'm a {this.props.name}! I'm  {this.props.age}years old </p>
+      <p key="i2">{this.props.children}</p>
       <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}/>
-      ]
-    // </div>
+    </Aux>
     )
   }
 }
