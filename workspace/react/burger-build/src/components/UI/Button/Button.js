07bf44d6e529ddb7d27233ea8def93b39cd1dbaf
Koichi <koichi89592@gmail.com>
2020-11-17 17:49:56 +0900

    Add button's styles and methods

--- workspace/react/burger-build/src/components/UI/Button/Button.js
+++ workspace/react/burger-build/src/components/UI/Button/Button.js
@@ -4,8 +4,8 @@ import classes from './Button.css';
 
 const button = (props) => (
   <button
-    className={[classes.Button, classes[props.btnType]]}
-    onclick={props.clicked}
+    className={[classes.Button, classes[props.btnType]].join(' ')}
+    onClick={props.clicked}
   >{props.children}</button>
 );
 
