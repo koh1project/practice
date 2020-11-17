d501ac7cc0931d7866c59caad7daab556ab2ee8f
Koichi <koichi89592@gmail.com>
2020-11-17 13:33:21 +0900

    Add order cancel method

--- workspace/react/burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
+++ workspace/react/burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from'../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal';
+import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
@@ -76,6 +76,10 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: true});
   }
 
+  purchaseCancelHandler = () => {
+    this.setState({purchasing: false});
+  }
+
   render(){
     const disabledInfo = {
       ...this.state.ingredients
@@ -87,7 +91,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
