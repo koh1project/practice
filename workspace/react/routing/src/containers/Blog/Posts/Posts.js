cebe061295ef30784c765976ab126e3551746754
Koichi <koichi89592@gmail.com>
2020-11-20 21:33:59 +0900

    Make a nested route

--- workspace/react/routing/src/containers/Blog/Posts/Posts.js
+++ workspace/react/routing/src/containers/Blog/Posts/Posts.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-// import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css'
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
   state = {
@@ -52,9 +53,12 @@ class Posts extends Component {
     }
 
     return (
-      <section className="Posts">
-        {posts}
-    </section>
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <Route path="/:id" exact  component={FullPost} />
+      </div>
     );
   }
 }
