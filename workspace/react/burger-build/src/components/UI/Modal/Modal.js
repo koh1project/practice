2665bc01d8e0357cd1336b2881295d15db73916f
Koichi <koichi89592@gmail.com>
2020-11-20 17:08:35 +0900

    Add spinner

--- workspace/react/burger-build/src/components/UI/Modal/Modal.js
+++ workspace/react/burger-build/src/components/UI/Modal/Modal.js
@@ -8,7 +8,7 @@ class  Modal extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     // console.log(`[${this.constructor.name}] shouldComponentUpdate`);
-      return nextProps.show !== this.props.show;
+      return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
   componentWillUpdate() {
