f522da62d371c306db15d5633bb4ea29fd71f202
Koichi <koichi89592@gmail.com>
2020-11-20 21:48:44 +0900

    Fix bug

--- workspace/react/routing/src/containers/Blog/FullPost/FullPost.js
+++ workspace/react/routing/src/containers/Blog/FullPost/FullPost.js
@@ -9,8 +9,17 @@ class FullPost extends Component {
   }
 
   componentDidMount () {
+    this.loadData();
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    this.loadData();
+  }
+
+  loadData () {
     if (this.props.match.params.id) {
-      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+      if (!this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== Number.parseInt(this.props.match.params.id , 10))) {
         axios.get('/posts/' + this.props.match.params.id)
           .then(response => {
             // console.log(response);
@@ -21,14 +30,14 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete('/posts/' + this.props.id)
+    axios.delete('/posts/' + this.props.match.params.id)
       .then(response => {
         console.log(response);
       });
   }
   render () {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
-    if ( this.props.id ) {
+    if ( this.props.match.params.id ) {
       post = <p style={{ textAlign: 'center' }}>Loading...!</p>;
     }
     if ( this.state.loadedPost ) {
