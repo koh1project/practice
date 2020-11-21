8394deab578dfcfe4c08cc2e55836afd76c27ecb
Koichi <koichi89592@gmail.com>
2020-11-21 18:35:48 +0900

    Fix error

--- workspace/react/burger-build/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ workspace/react/burger-build/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -13,10 +13,10 @@ const checkoutSummary = (props) => {
       </div>
       <Button
         btnType="Danger"
-        clicked>CANCEL</Button>
+        clicked={() => {}}>CANCEL</Button>
       <Button
         btnType="Success"
-        clicked>CONTINUE</Button>
+        clicked={() => {}}>CONTINUE</Button>
     </div>
   );
 };
