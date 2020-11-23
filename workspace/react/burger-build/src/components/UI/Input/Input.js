64168667c6fc3c41a8fce1e08b144466166ede30
Koichi <koichi89592@gmail.com>
2020-11-23 16:51:53 +0900

    Add dynamic generate input

--- workspace/react/burger-build/src/components/UI/Input/Input.js
+++ workspace/react/burger-build/src/components/UI/Input/Input.js
@@ -5,15 +5,36 @@ import classes from './Input.css'
 const input = (props) => {
   let InputElement = null;
 
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case ('input'):
-      InputElement = <input className={classes.InputElement} {...props}/>;
+      InputElement = <input
+       className={classes.InputElement}
+       {...props.elementConfig}
+        value={props.value} />;
       break;
     case ('textarea'):
-      InputElement = <textarea className={classes.InputElement} {...props}/>
+      InputElement = <textarea
+        className={classes.InputElement}
+        {...props.elementConfig}
+        value={props.value} />;
+      break;
+    case ('select'):
+      InputElement =
+      <select
+        className={classes.InputElement}
+        value={props.value}>
+        {props.elementConfig.options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.displayValue}
+          </option>
+        ))}
+      </select>;
       break;
     default:
-      InputElement = <input  className={classes.InputElement} {...props} />
+      InputElement = <input
+        className={classes.InputElement}
+        {...props.elementConfig}
+        value={props.value} />;
       break;
   }
 
--- workspace/react/burger-build/src/containers/Checkout/ContactData/ContactData.js
+++ workspace/react/burger-build/src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your mail address'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'},
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
   };
@@ -22,17 +68,7 @@ class ContactData extends Component {
       this.setState({loading: true});
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price,
-      customer: {
-        name: 'This is my name',
-        address: {
-          street: 'Test street 1',
-          zipCode: '123456',
-          country: 'Japan'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
+      price: this.props.price
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -45,13 +81,25 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElementsArray = [];
+    for(let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (
       <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name"/>
-        <Input inputtype="input" type="email" name="email" placeholder="Your Mail"/>
-        <Input inputtype="input" type="text" name="street" placeholder="Street"/>
-        <Input inputtype="input" type="text" name="postalCode" placeholder="Postal Code"/>
-      <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        <Input elementType="..." elementConfig="" value="..."/>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value} />
+        ))}
+        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
     </form>
     );
     if (this.state.loading) {
