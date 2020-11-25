8889aec699c91637b69eb7e1b2f7f8990df8f8b0
Koichi <koichi89592@gmail.com>
2020-11-25 19:23:02 +0900

    Change add person

--- workspace/react/redux--assignment-2-problem/src/App.js
+++ workspace/react/redux--assignment-2-problem/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Persons from './containers/Persons';
 
+
 class App extends Component {
   render() {
     return (
--- workspace/react/redux--assignment-2-problem/src/containers/Persons.js
+++ workspace/react/redux--assignment-2-problem/src/containers/Persons.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
-
+import * as actionTypes from '../store/actions';
 class Persons extends Component {
   state = {
     persons: []
@@ -26,15 +27,26 @@ class Persons extends Component {
   }
 
   render() {
-    return (<div>
-      <AddPerson personAdded = { this.personAddedHandler }/> 
-      {this.state.persons.map(person => (
-        <Person key = {person.id} name = {person.name} age = {person.age}
-        clicked = {() => this.personDeletedHandler(person.id) }
-        />))}
+    return (
+      <div>
+        <AddPerson personAdded={this.props.onAddPerson}/>
+        {this.props.psn.map(person => (
+          <Person key = {person.id} name = {person.name} age = {person.age}
+          clicked = {() => this.personDeletedHandler(person.id) }
+          />))}
       </div>
     );
   }
 }
 
-export default Persons;
+const mapStateToProps = state => ({
+  psn: state.persons
+});
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON})
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
