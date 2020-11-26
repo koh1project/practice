8c98c4436a943354c68f60f15c4e4b29a1eaa7d4
Koichi <koichi89592@gmail.com>
2020-11-26 17:46:07 +0900

    Enable order button activating

--- workspace/react/burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
+++ workspace/react/burger-build/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -46,7 +46,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
          return sum + el
       } , 0);
-      this.setState({purchasable: sum > 0});
+      return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -90,7 +90,7 @@ class BurgerBuilder extends Component {
            ingredientAdded={this.props.onIngredientAdded}
            ingredientRemoved={this.props.onIngredientRemoved}
            disabled={disabledInfo}
-           purchasable={this.state.purchasable}
+           purchasable={this.updatePurchaseState(this.props.ings)}
            price={this.props.price}
            ordered={this.purchaseHandler} />
        </Aux>
