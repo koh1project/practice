86f366dac4ea6bbe1920db0866e0c1db35d91f5f
Koichi <koichi89592@gmail.com>
2020-12-02 18:05:28 +0900

    Use local storage

--- workspace/react/burger-build/src/store/actions/auth.js
+++ workspace/react/burger-build/src/store/actions/auth.js
@@ -8,7 +8,6 @@ export const authStart = () => {
 };
 
 export const authSuccess = (token, userId) => {
-
   return {
     type: actionTypes.AUTH_SUCCESS,
     token: token,
@@ -18,7 +17,8 @@ export const authSuccess = (token, userId) => {
 
 export const logout = () => {
   localStorage.removeItem('token');
-  localStorage.removeItem('expirationTime');
+  localStorage.removeItem('expirationDate');
+  localStorage.removeItem('userId');
   return {
     type: actionTypes.AUTH_LOGOUT
   };
@@ -62,7 +62,8 @@ export const auth = (email, password, isSignup) => {
         console.log(response);
         const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
         localStorage.setItem('token', response.data.idToken);
-        localStorage.setItem('expirationDate', expirationDate)
+        localStorage.setItem('expirationDate', expirationDate);
+        localStorage.setItem('userId', response.data.localId);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
@@ -77,4 +78,23 @@ export const setAuthRedirectPath = (path) => {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
   }
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
+        const userId = localStorage.getItem('userId');
+        dispatch(authSuccess(token, userId));
+        dispatch(checkAuthTimeout(expirationDate.getTime() - new Date().getTime() / 1000));
+      }
+    }
+
+  };
 };
--- workspace/react/burger-build/src/store/actions/index.js
+++ workspace/react/burger-build/src/store/actions/index.js
@@ -17,6 +17,7 @@ from './order';
 export {
   auth,
   logout,
-  setAuthRedirectPath
+  setAuthRedirectPath,
+  authCheckState
 }
 from './auth';
