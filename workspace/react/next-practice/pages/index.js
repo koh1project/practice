76d142e852cc5d09366fea5566f1058aef9d301b
Koichi <koichi89592@gmail.com>
2020-12-05 17:52:14 +0900

    Use Life Cycle Hook

--- workspace/react/next-practice/pages/index.js
+++ workspace/react/next-practice/pages/index.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-  <div>
-    <h1>The Main Page</h1>
-    <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-    <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-  </div>
-);
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({appName: 'Return app'});
+      }, 1000);
+    });
 
-export default indexPage;
+    return promise;
+  }
+
+  render() {
+   return (
+      <div>
+        <h1>This props of {this.props.appName}</h1>
+        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
