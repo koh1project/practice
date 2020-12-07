93bf1f32a96c754d7cf3b4b62f1abb8bcd616a91
Koichi <koichi89592@gmail.com>
2020-12-07 19:07:05 +0900

    Remove an unnecessary block

--- workspace/react/hooks-practice/src/components/Ingredients/Ingredients.js
+++ workspace/react/hooks-practice/src/components/Ingredients/Ingredients.js
@@ -8,22 +8,6 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   useEffect(() => {
-    fetch('https://react-hooks-practice-8c8e5-default-rtdb.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    });
-  }, []);
-
-  useEffect(() => {
     console.log(`Re-render userIngredients`, userIngredients);
   }, [userIngredients]);
 
