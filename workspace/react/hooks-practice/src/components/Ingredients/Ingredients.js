dd691c7ab83e55633f763b6e1daeda1b2fc29cd6
Koichi <koichi89592@gmail.com>
2020-12-07 19:32:50 +0900

    Add loading spinner

--- workspace/react/hooks-practice/src/components/Ingredients/Ingredients.js
+++ workspace/react/hooks-practice/src/components/Ingredients/Ingredients.js
@@ -6,6 +6,7 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     console.log(`Re-render userIngredients`, userIngredients);
@@ -16,11 +17,14 @@ const Ingredients = () => {
   }, []);
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
+
     fetch('https://react-hooks-practice-8c8e5-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {'Content-Type': 'application/json'}
     }).then(response => {
+      setIsLoading(false);
       return response.json();
     }).then(responseData => {
       setUserIngredients(prevIngredients => [
@@ -31,9 +35,11 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true);
     fetch(`https://react-hooks-practice-8c8e5-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE'
     }).then(response => {
+      setIsLoading(false);
       setUserIngredients(prevIngredients =>
         prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
       )
@@ -42,12 +48,15 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}/>
 
       <section>
-        <Search
-        onLoadIngredients={filteredIngredientHandler}/>
-        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
+        <Search onLoadIngredients={filteredIngredientHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}/>
         {/* Need to add list here! */}
       </section>
     </div>
