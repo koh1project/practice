7c142b297857231a3a881b940e0ca768129d953b
Koichi <koichi89592@gmail.com>
2020-12-09 17:37:16 +0900

    Replace functional to class-based

--- workspace/typescript/rrts/src/index.tsx
+++ workspace/typescript/rrts/src/index.tsx
@@ -5,30 +5,26 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
 
-// class App extends React.Component<AppProps> {
-//   state = { counter: 0 };
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
-
-ReactDOM.render(<App color="red"/>, document.querySelector('#root'));
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
