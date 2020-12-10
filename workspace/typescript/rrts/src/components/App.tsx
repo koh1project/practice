dfc75dc1097b5678fca2b04463cb80095b4d5929
Koichi <koichi89592@gmail.com>
2020-12-10 19:08:53 +0900

    Add loading state

--- workspace/typescript/rrts/src/components/App.tsx
+++ workspace/typescript/rrts/src/components/App.tsx
@@ -3,10 +3,26 @@ import { connect, ConnectedProps } from 'react-redux';
 import { Todo, fetchTodos, deleteTodo } from '../actions';
 import { StoreState } from '../reducers';
 
-class _App extends Component<AppProps> {
+interface AppState {
+  fetching: boolean;
+}
+
+class _App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { fetching: false };
+  }
+
   onButtonClick = (): void => {
     this.props.fetchTodos();
+    this.setState({ fetching: true });
   };
+
+  componentDidUpdate(prevProps: AppProps): void {
+    if (!prevProps.todos.length && this.props.todos.length) {
+      this.setState({ fetching: false });
+    }
+  }
 
   onTodoClick = (id: number): void => {
     this.props.deleteTodo(id);
@@ -26,6 +42,7 @@ class _App extends Component<AppProps> {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.state.fetching ? 'Loading' : null}
         {this.renderList()}
       </div>
     );
