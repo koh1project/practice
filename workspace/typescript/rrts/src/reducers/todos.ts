6088ad0e98a5699d16ad6b325ecb3fcdead1cec7
Koichi <koichi89592@gmail.com>
2020-12-10 17:24:31 +0900

    Fix bug

--- workspace/typescript/rrts/src/reducers/todos.ts
+++ workspace/typescript/rrts/src/reducers/todos.ts
@@ -2,12 +2,10 @@ import { Todo, FetchTodosAction } from '../actions';
 import { ActionTypes } from '../actions/types';
 
 export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
-  // switch (action.type) {
-  //   case ActionTypes.fetchTodos:
-  //     return action.payload;
-  //   default:
-  //     return state;
-  // }
-
-  return 5;
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    default:
+      return state;
+  }
 };
