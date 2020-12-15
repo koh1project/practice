908c9397597d55efc3d56cdcaea5b03d370cb4c0
Koichi <koichi89592@gmail.com>
2020-12-15 12:31:11 +0900

    Styling menu-component

--- workspace/react/crwn-clothing/src/components/directory/directory.component.jsx
+++ workspace/react/crwn-clothing/src/components/directory/directory.component.jsx
@@ -51,7 +51,7 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={title} title={id} imageUrl={imageUrl} size={size} />
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
