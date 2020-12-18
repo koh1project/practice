f45b598012544a141aca1d8145e58b9833619c92
Koichi <koichi89592@gmail.com>
2020-12-18 13:08:51 +0900

    Add firestore query

--- workspace/react/crwn-clothing/src/firebase/firebase.utils.js
+++ workspace/react/crwn-clothing/src/firebase/firebase.utils.js
@@ -22,3 +22,30 @@ provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.id}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error('Error creating user', error.message);
+    }
+  }
+  // eslint-disable-next-line consistent-return
+  return userRef;
+};
