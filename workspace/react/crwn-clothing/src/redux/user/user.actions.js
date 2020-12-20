70d23bfb8b62c9eb2d513f3a20289b3a4de83692
Koichi <koichi89592@gmail.com>
2020-12-20 16:58:01 +0900

    Set up redux

---
+++ workspace/react/crwn-clothing/src/redux/user/user.actions.js
@@ -0,0 +1,9 @@
+// export const setCurrentUser = (user) => ({
+//   type: 'SET_CURRENT_USER',
+//   payload: user,
+// });
+
+export default (user) => ({
+  type: 'SET_CURRENT_USER',
+  payload: user,
+});
