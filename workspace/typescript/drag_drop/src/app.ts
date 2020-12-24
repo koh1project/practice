971054eda52a55458fd06e5d2195917d5e5d9460
Koichi <koichi89592@gmail.com>
2020-12-24 17:21:41 +0900

    Add validation

--- workspace/typescript/drag_drop/src/app.ts
+++ workspace/typescript/drag_drop/src/app.ts
@@ -35,10 +35,39 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredManday = this.mandayInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredManday.trim().length === 0
+    ) {
+      alert('Invalid input value');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, Number.parseFloat(enteredManday)];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.mandayInputElement.value = '';
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, manday] = userInput;
+      console.log(title, desc, manday);
+
+      this.clearInputs();
+    }
   }
 
   private configure() {
