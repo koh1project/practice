9e4214b7dfa15b1c27ced9b4c6b8bbfa8f98339b
Koichi <koichi89592@gmail.com>
2020-12-27 18:55:01 +0900

    Make collection-preview component

--- workspace/react/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ workspace/react/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -4,7 +4,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.component.styles.scss';
 
-const PreviewCollection = ({ title, items }) => (
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
@@ -17,4 +17,4 @@ const PreviewCollection = ({ title, items }) => (
   </div>
 );
 
-export default PreviewCollection;
+export default CollectionPreview;
