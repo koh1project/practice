ec54f9c54af0c2c55e828913004238cd6b5a2ae8
Koichi <koichi89592@gmail.com>
2020-12-30 14:31:45 +0900

    Add shop data reducer

--- workspace/react/crwn-clothing/src/firebase/firebase.utils.js
+++ workspace/react/crwn-clothing/src/firebase/firebase.utils.js
@@ -73,6 +73,8 @@ export const convertCollectionsSnapshotToMap = (collections) => {
       items,
     };
   });
-
-  console.log(transformedCollection);
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
