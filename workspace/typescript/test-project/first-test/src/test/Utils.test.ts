6437ea88b4a60775336b66dfb59ea55e5fb0869b
Koichi <koichi89592@gmail.com>
2021-01-02 18:43:59 +0900

    Make basic test

---
+++ workspace/typescript/test-project/first-test/src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils test suite', () => {
+  test('first test', () => {
+    const abc = Utils.toUpperCase('act');
+    console.log('First test')
+  });
+});
