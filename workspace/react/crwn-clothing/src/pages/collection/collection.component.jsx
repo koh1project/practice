4957ba6768238e99a0f87a8cd30ca305052d855c
Koichi <koichi89592@gmail.com>
2021-01-03 14:57:26 +0900

    Mimic componentWIllUnmount using useEffect

--- workspace/react/crwn-clothing/src/pages/collection/collection.component.jsx
+++ workspace/react/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import { firestore } from '../../firebase/firebase.utils';
 
 import {
   CollectionPageContainer,
@@ -12,6 +14,16 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
+  useEffect(() => {
+    const unsubscribeFromCollections = firestore
+      .collection('collections')
+      .onSnapshot(async (snapshot) => console.log(snapshot));
+
+    return () => {
+      unsubscribeFromCollections();
+    };
+  }, []);
+
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
