57784d1ea394f57cd644e8053a6fd4fe885da12e
Koichi <koichi89592@gmail.com>
2021-01-03 14:58:01 +0900

    Reset collection.component

--- workspace/react/crwn-clothing/src/pages/collection/collection.component.jsx
+++ workspace/react/crwn-clothing/src/pages/collection/collection.component.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
-
-import { firestore } from '../../firebase/firebase.utils';
 
 import {
   CollectionPageContainer,
@@ -14,16 +12,6 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
-  useEffect(() => {
-    const unsubscribeFromCollections = firestore
-      .collection('collections')
-      .onSnapshot(async (snapshot) => console.log(snapshot));
-
-    return () => {
-      unsubscribeFromCollections();
-    };
-  }, []);
-
   const { title, items } = collection;
   return (
     <CollectionPageContainer>
