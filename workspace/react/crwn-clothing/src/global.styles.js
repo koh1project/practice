cbcd32eb9c2120bb5264d8f55e32e7fb076bab1c
Koichi <koichi89592@gmail.com>
2021-01-05 21:02:10 +0900

    Add media queries

--- workspace/react/crwn-clothing/src/global.styles.js
+++ workspace/react/crwn-clothing/src/global.styles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle = createGlobalStyle `
+export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans Condensed';
     padding: 20px 40px;
