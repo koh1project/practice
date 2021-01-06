c58afbdf7e593e9872d7288be821e6b371461db8
Koichi <koichi89592@gmail.com>
2021-01-06 20:48:48 +0900

    Add grouping with describe

--- workspace/react/testing/script.test.js
+++ workspace/react/testing/script.test.js
@@ -7,11 +7,22 @@ dbMock = [
   'dogpicture.com',
 ]
 
-it('This is a test', () => {
-  expect('Hello').toBe('Hello');
-});
+describe('googleSearch', () => {
+  it('This is a test', () => {
+    expect('Hello').toBe('Hello');
+  });
 
-it('It is searching google', () => {
-  expect(googleSearch('test', dbMock)).toEqual([]);
-  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpicture.com']);
+  it('It is searching google', () => {
+    expect(googleSearch('test', dbMock)).toEqual([]);
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpicture.com']);
+  });
+
+  it('Work with undefined and null input', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it('Does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+  });
 });
