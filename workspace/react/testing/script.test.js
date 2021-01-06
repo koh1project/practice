d80a692e084ec52d6328ffd146a705d29bde46c3
Koichi <koichi89592@gmail.com>
2021-01-06 20:44:05 +0900

    Add some tests

--- workspace/react/testing/script.test.js
+++ workspace/react/testing/script.test.js
@@ -1,3 +1,17 @@
-it('This is a test', () => {
+const googleSearch = require('./script');
 
+dbMock = [
+  'dog.com',
+  'cheese.com',
+  'disney.com',
+  'dogpicture.com',
+]
+
+it('This is a test', () => {
+  expect('Hello').toBe('Hello');
+});
+
+it('It is searching google', () => {
+  expect(googleSearch('test', dbMock)).toEqual([]);
+  expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpicture.com']);
 });
