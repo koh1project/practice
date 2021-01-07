028056087d7e350bf4798a5e2dd018f6fe0b65e4
Koichi <koichi89592@gmail.com>
2021-01-07 19:08:43 +0900

    Add a mock test

--- workspace/react/testing/src/components/MainPage.test.js
+++ workspace/react/testing/src/components/MainPage.test.js
@@ -38,4 +38,20 @@ it('filters robots correctly', () => {
   }]);
 });
 
+it('filters robots correctly 2', () => {
+  const mockProps3 = {
+    onRequestRobots: jest.fn(),
+    robots:[{
+      id: 3,
+      name: 'john',
+      email: 'john@gmail.com'
+    }],
+    searchField: 'a',
+    isPending: false,
+  };
+  const filterRobots = [];
+  const wrapper3 = shallow(<MainPage {...mockProps3}/>);
+  expect(wrapper3.instance().filterRobots([])).toEqual(filterRobots);
+});
+
 
