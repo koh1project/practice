4f4c1ce583a6c3f39a29c21f4e61fcd9b332a1e0
Koichi <koichi89592@gmail.com>
2021-01-07 18:40:35 +0900

    Add tests for connectiong among components

---
+++ workspace/react/testing/src/components/MainPage.test.js
@@ -0,0 +1,41 @@
+import { shallow } from 'enzyme'
+import React from 'react';
+import MainPage from './MainPage';
+
+let wrapper;
+
+beforeEach(() => {
+  const mockProps = {
+    onRequestRobots: jest.fn(),
+    robots:[],
+    searchField: '',
+    isPending: false,
+  };
+  wrapper = shallow(<MainPage {...mockProps}/>);
+});
+
+it('renders MainPage without crashing', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('filters robots correctly', () => {
+  const mockProps2 = {
+    onRequestRobots: jest.fn(),
+    robots:[{
+      id: 3,
+      name: 'john',
+      email: 'john@gmail.com'
+    }],
+    searchField: 'john',
+    isPending: false,
+  };
+  const wrapper2 = shallow(<MainPage {...mockProps2}/>);
+
+  expect(wrapper2.instance().filterRobots([])).toEqual([{
+    id: 3,
+    name: 'john',
+    email: 'john@gmail.com'
+  }]);
+});
+
+
