487b65f4bf98e8723e92aa3654a606338197318e
Koichi <koichi89592@gmail.com>
2021-01-08 17:57:34 +0900

    Cause an error

--- workspace/react/crwn-clothing/src/pages/homepage/homepage.component.jsx
+++ workspace/react/crwn-clothing/src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,15 @@ import Directory from '../../components/directory/directory.component';
 
 import { HomePageContainer } from './homepage.styles';
 
-const HomePage = () => (
-  <HomePageContainer>
-    <Directory />
-  </HomePageContainer>
-);
+const HomePage = () => {
+  throw Error;
+
+  // eslint-disable-next-line no-unreachable
+  return (
+    <HomePageContainer>
+      <Directory />
+    </HomePageContainer>
+  );
+};
 
 export default HomePage;
