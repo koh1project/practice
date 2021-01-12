b6c85e8b127faedba577f6ee5d587e617c8b6a3b
Koichi <koichi89592@gmail.com>
2021-01-12 23:09:54 +0900

    Add db integration tests

---
+++ workspace/typescript/test-project/test-doubles/src/itest/SessionTokenDBAccess.spec.ts
@@ -0,0 +1,38 @@
+import { SessionTokenDBAccess } from './../app/Authorization/SessionTokenDBAccess';
+import { SessionToken } from './../app/Models/ServerModels';
+
+describe('First itest suite', () => {
+  let sessionTokenDBAccess: SessionTokenDBAccess;
+  let someSessionToken: SessionToken;
+  const randomString = Math.random().toString(36).substring(7);
+
+  beforeAll(() => {
+    sessionTokenDBAccess = new SessionTokenDBAccess();
+    someSessionToken = {
+      tokenId: 'someTokenId' + randomString,
+      userName: 'someUserName',
+      valid: true,
+      expirationTime: new Date(),
+      accessRights: [1, 2, 3],
+    };
+  });
+
+  test('store and retrieve SessionToken', async() => {
+    await sessionTokenDBAccess.storeSessionToken(someSessionToken);
+    const resultToken = await sessionTokenDBAccess.getToken(someSessionToken.tokenId);
+    expect(resultToken).toMatchObject(someSessionToken);
+  });
+  test('delete sessionToken', async () => {
+    await sessionTokenDBAccess.deleteToken(someSessionToken.tokenId);
+    const resultToken = await sessionTokenDBAccess.getToken(someSessionToken.tokenId);
+    expect(resultToken).toBeUndefined();
+  });
+  test('delete missing sessionToken throws error', async () => {
+    try {
+      await sessionTokenDBAccess.deleteToken(someSessionToken.tokenId);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'SessionToken not deleted!');
+    }
+  });
+});
