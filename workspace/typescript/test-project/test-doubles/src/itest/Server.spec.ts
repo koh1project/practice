0a3615879ec0b854baeeaa79601af62be2792f39
Koichi <koichi89592@gmail.com>
2021-01-13 13:57:08 +0900

    Add server asynchronous tests

--- workspace/typescript/test-project/test-doubles/src/itest/Server.spec.ts
+++ workspace/typescript/test-project/test-doubles/src/itest/Server.spec.ts
@@ -1,16 +1,77 @@
+import { UserCredentialsDbAccess } from './../app/Authorization/UserCredentialsDbAccess';
+import { HTTP_CODES, UserCredentials, SessionToken } from './../app/Models/ServerModels';
 import * as axios from 'axios';
 
 
 axios.default.defaults.validateStatus = function () {
   return true;
-};
+}
 const serverUrl = 'http://localhost:8080';
+const itestUserCredentials: UserCredentials = {
+  accessRights: [1, 2, 3],
+  password: 'iTestPassword',
+  username: 'iTestUser'
+}
 
-describe('Server itest suite', async() => {
-  const testIfServerReachable = await serverReachable ?  test : test.skip;
-  test('server reachable', async() => {
-    await serverReachable();
+describe('Server itest suite', () => {
+  let userCredentialsDBAccess: UserCredentialsDbAccess;
+  let sessionToken: SessionToken;
+
+  beforeAll(() => {
+    userCredentialsDBAccess = new UserCredentialsDbAccess();
   });
+
+  test('server reachable', async () => {
+    const response = await axios.default.options(serverUrl);
+    expect(response.status).toBe(HTTP_CODES.OK);
+});
+test.skip('put credentials inside database', async () => {
+    await userCredentialsDBAccess.putUserCredential(itestUserCredentials);
+});
+test('reject invalid credentials', async () => {
+    const response = await axios.default.post(
+        (serverUrl + '/login'),
+        {
+            "username": "someWrongCred",
+            "password": "someWrongCred"
+        }
+    );
+    expect(response.status).toBe(HTTP_CODES.NOT_fOUND);
+});
+  test('login successful with correct credentials', async () => {
+    const response = await axios.default.post(
+        (serverUrl + '/login'),
+        {
+            "username": itestUserCredentials.username,
+            "password": itestUserCredentials.password
+        }
+    );
+    expect(response.status).toBe(HTTP_CODES.CREATED);
+    sessionToken = response.data;
+});
+test('Query data', async () => {
+  const response = await axios.default.get(
+    (serverUrl + '/users?name=some'), {
+      headers: {
+        Authorization: sessionToken.tokenId
+      }
+    }
+  );
+  expect(response.status).toBe(HTTP_CODES.OK);
+});
+test('Query data with invalid token', async () => {
+  const response = await axios.default.get(
+    (serverUrl + '/users?name=some'), {
+      headers: {
+        Authorization: sessionToken.tokenId + 'someStuff'
+      }
+    }
+  );
+  expect(response.status).toBe(HTTP_CODES.UNAUTHORIZED);
+});
+
+
+
 });
 
 /**
