cecab52dec369de72ffed025c368bcfb0a026126
Koichi <koichi89592@gmail.com>
2021-01-15 07:58:23 +0900

    Add styles

--- workspace/typescript/develop-project/rrts/src/components/TodoList.tsx
+++ workspace/typescript/develop-project/rrts/src/components/TodoList.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+
+import './TodoList.css';
+
 interface TodoListProps {
   items: { id: string; text: string }[];
   onDeleteTodo: (id: string) => void;
 }
-
-
 
 const TodoList: React.FC<TodoListProps> = (props) => {
   return (
