0f1341bf4bbf8e2dce217a5ddb4acc041533efb7
Koichi <koichi89592@gmail.com>
2021-01-14 18:58:17 +0900

    Add input handler

--- workspace/typescript/develop-project/rrts/src/App.tsx
+++ workspace/typescript/develop-project/rrts/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'todo task' }];
 
   return (
-    <div className="App">
+    <div className='App'>
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
