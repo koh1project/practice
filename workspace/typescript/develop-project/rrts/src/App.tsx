6f55253895f0f7588af88ff908d9aa75c744c92a
Koichi <koichi89592@gmail.com>
2021-01-14 19:07:07 +0900

    Use setState

--- workspace/typescript/develop-project/rrts/src/App.tsx
+++ workspace/typescript/develop-project/rrts/src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewTodo from './components/NewTodo';
 import TodoList from './components/TodoList';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-
-  const todos = [{ id: 't1', text: 'todo task' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
 
   return (
