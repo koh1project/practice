2fde91de4535b2b96d03d9adc90286baf7d9e04e
Koichi <koichi89592@gmail.com>
2021-01-15 18:38:51 +0900

    Add a enzyme test

--- workspace/react-testing/demo/src/App.test.js
+++ workspace/react-testing/demo/src/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const wrapper = shallow(<App />);
 });
