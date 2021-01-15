0608754d297d8f431e6e6b937f598f9e49f9cece
Koichi <koichi89592@gmail.com>
2021-01-15 09:05:32 +0900

    Add map display with openlayers

--- workspace/typescript/develop-project/maps/src/app.ts
+++ workspace/typescript/develop-project/maps/src/app.ts
@@ -1,1 +1,29 @@
-// ここにコードを書きます
+declare var ol: any;
+
+// import axios from "axios";
+
+const form = document.querySelector("form")!;
+// const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const searchAddressHandler = (event: Event) => {
+  event.preventDefault();
+
+  const coordinates = { lat: 40.41, lng: -73.99 };
+
+
+  document.getElementById("map")!.innerHTML = "";
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+      zoom: 16
+    })
+  });
+};
+
+form.addEventListener("submit", searchAddressHandler);
