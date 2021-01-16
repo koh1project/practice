ba1f1efeb517c728db9651865662cf341c51982c
Koichi <koichi89592@gmail.com>
2021-01-16 18:12:05 +0900

    Refactor

--- workspace/react-testing/demo/src/App.test.js
+++ workspace/react-testing/demo/src/App.test.js
@@ -1,34 +1,43 @@
 import React from 'react'
 import App from './App';
 
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ * Factory function to create ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`);
+
 test('renders without error', () => {
-  const wrapper = shallow( < App / > );
-  const appComponent = wrapper.find('[data-test="component-app"]');
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent.length).toBe((1));
 });
 test('renders button', () => {
-  const wrapper = shallow( < App / > );
-  const button = wrapper.find('[data-test="increment-button"]');
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
   expect(button.length).toBe((1));
 });
 test('renders counter display', () => {
-  const wrapper = shallow( < App / > );
-  const counterDisplay = wrapper.find('[data-test="counter-display"]');
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
   expect(counterDisplay.length).toBe((1));
 });
 test('counter starts at 0', () => {
-  const wrapper = shallow( < App / > );
-  const appComponent = wrapper.find('[data-test="component-app"]');
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, '');
   expect(appComponent.length).toBe((1));
 });
 test('clicking on button increments counter display', () => {
-  const wrapper = shallow( < App / > );
-  const appComponent = wrapper.find('[data-test="component-app"]');
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, '');
   expect(appComponent.length).toBe((1));
 });
