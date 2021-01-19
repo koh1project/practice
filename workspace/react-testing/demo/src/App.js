763e46352e1087e35422d68634eef342b238e395
Koichi <koichi89592@gmail.com>
2021-01-19 19:22:20 +0900

    Add bootstrap styling

--- workspace/react-testing/demo/src/App.js
+++ workspace/react-testing/demo/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
-  return (
-    <div></div>
-  );
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
+
+
+
+export class App extends Component {
+  render() {
+    return ( <
+      div className = "container" >
+      <
+      h1 > Jotto < /h1> <
+      Congrats success = { true }
+      /> <
+      GuessedWords guessedWords = {
+        [
+          { guessedWord: 'train', letterMatchCount: 3 }
+        ]
+      }
+      /> <
+      /div>
+    );
+  }
 }
 
 export default App;
