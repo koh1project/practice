784e891823e5a5e8efc6062ab23fc6f83b95428f
Koichi <koichi89592@gmail.com>
2021-01-19 23:56:26 +0900

    Write code and tests for successReducer

--- workspace/react-testing/demo/src/reducers/successReducer.js
+++ workspace/react-testing/demo/src/reducers/successReducer.js
@@ -1,3 +1,11 @@
-export default ( state, action) => {
-  return null
+/* eslint-disable import/no-anonymous-default-export */
+import { actionTypes } from "../actions";
+
+export default (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS :
+      return true;
+    default :
+    return state;
+  }
 };
