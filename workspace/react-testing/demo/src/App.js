a77fc081cadfd9480fad2fb16e96b656de313591
Koichi <koichi89592@gmail.com>
2021-01-22 08:04:21 +0900

    Basic App component

--- workspace/react-testing/demo/src/App.js
+++ workspace/react-testing/demo/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div/>
+  return (
+    <div data-test='component-app'>
+    </div>
+  )
 }
 
 export default App;
