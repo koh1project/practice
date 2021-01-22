9a4ffff0841311b45b4f4c0b138e64c3179eef63
Koichi <koichi89592@gmail.com>
2021-01-22 17:05:39 +0900

    Set up tests for Input component

--- workspace/react-testing/demo/src/Input.js
+++ workspace/react-testing/demo/src/Input.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-const Input = () => {
+const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('');
 
   return(
     <div data-test='component-input'>
+      <form className="form-inline">
+        <input
+          data-test='input-box'
+          className='mb-2 mx-sm-3'
+          type='text'
+          placeholder='enter guess'
+          value={currentGuess}
+          onChange={(event) => setCurrentGuess(event.target.value)}
+        />
+        <button
+          data-test='submit-button'
+          onClick={(evt) => {
+            evt.preventDefault();
+            //TODO: update guessedWords
+            //TODO: check against secretWord and update success if needed
+            setCurrentGuess('')
+          }}
+          className='bnt btn-primary mb-2'
+        >Submit</button>
+      </form>
     </div>
   );
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
