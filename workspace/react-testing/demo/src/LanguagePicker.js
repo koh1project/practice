8e0ee1a1188a88443578ffd5b45fe4ce24a795b0
Koichi <koichi89592@gmail.com>
2021-01-29 09:38:10 +0900

    Make and Test LanguagePicker

---
+++ workspace/react-testing/demo/src/LanguagePicker.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import propTypes from 'prop-types';
+
+const LanguagePicker = ({ setLanguage }) => {
+  const languages = [
+    { code: 'en', symbol: '🎯' },
+    { code: 'emoji', symbol: '🤷‍' },
+  ];
+
+  const languageIcons = languages.map(lang =>
+    <span
+      data-test="language-icon"
+      key={lang.code}
+      onClick={() => setLanguage(lang.code)}
+    >
+      {lang.symbol}
+    </span>
+  );
+
+  return (
+    <div data-test='component-language-picker'>
+      {languageIcons}
+    </div>
+  );
+};
+
+LanguagePicker.propTypes = {
+  setLanguage: propTypes.func.isRequired,
+};
+
+export default LanguagePicker;
