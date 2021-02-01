231f8d18ddb428072312a9dcb4cdd65ba3703498
Koichi <koichi89592@gmail.com>
2021-02-01 16:55:28 +0900

    Make MovieList component

--- workspace/next/movie-app/pages/index.js
+++ workspace/next/movie-app/pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Navbar from '../components/navbar';
 import SideMenu from '../components/sideMenu';
 import Carousel from '../components/carousel';
+import MovieList from '../components/movieList';
 
 
 const Home = () => (
@@ -14,123 +15,20 @@ const Home = () => (
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
     </Head>
-
     <Navbar />
-
     <div className="container">
       <div className="row">
-
         <div className="col-lg-3">
-
         <SideMenu />
-
         </div>
-
         <div className="col-lg-9">
           <Carousel />
-
           <div className="row">
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item One</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item Two</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item Three</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item Four</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item Five</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 mb-4">
-              <div className="card h-100">
-                <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
-                <div className="card-body">
-                  <h4 className="card-title">
-                    <a href="#">Item Six</a>
-                  </h4>
-                  <h5>$24.99</h5>
-                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                </div>
-              </div>
-            </div>
+            <MovieList />
           </div>
         </div>
       </div>
     </div>
-
     <footer className="py-5 bg-dark">
       <div className="container">
         <p className="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
