29e20abcded5a033361534ca625a1d3ee8ab6140
Koichi <koichi89592@gmail.com>
2021-02-03 21:57:15 +0900

    Add movie data and use Array.map

--- workspace/next/movie-app/components/sideMenu.js
+++ workspace/next/movie-app/components/sideMenu.js
@@ -1,21 +1,12 @@
 const SideMenu = (props) => {
-  const { count } = props;
   return(
     <div>
-      <button onClick={props.clickHandler}>Click ME!</button>
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         <a href="#" className="list-group-item">Category 1</a>
         <a href="#" className="list-group-item">Category 2</a>
         <a href="#" className="list-group-item">Category 3</a>
       </div>
-      <div>
-
-        <div>
-          <h1>{count}</h1>
-        </div>
-      </div>
-
     </div>
   );
 };
