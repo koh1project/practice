38061e6579d5fbac15d5fefbe7cced0ff3a33a28
Koichi <koichi89592@gmail.com>
2021-02-13 14:17:58 +0900

    Set redux-thunk

--- workspace/react/crwn-clothing/src/components/header/header.component.jsx
+++ workspace/react/crwn-clothing/src/components/header/header.component.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
-import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
-const Header = ({ currentUser, hidden, signOutStart }) => (
+const Header = ({ currentUser, hidden }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className="logo" />
@@ -21,7 +21,7 @@ const Header = ({ currentUser, hidden, signOutStart }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink as="div" onClick={signOutStart}>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
           SIGN OUT
         </OptionLink>
       ) : (
@@ -38,8 +38,4 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  signOutStart: () => dispatch(signOutStart()),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps)(Header);
--- workspace/react/crwn-clothing/src/components/with-spinner/with-spinner.component.jsx
+++ workspace/react/crwn-clothing/src/components/with-spinner/with-spinner.component.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import Spinner from '../spinner/spinner.component';
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
 
-const WithSpinner = (WrappedComponent) => ({ isLoading, ...otherProps }) => {
-  return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
+const WithSpinner = (WrappedComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
 };
 
 export default WithSpinner;
