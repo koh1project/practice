433c3725bc2c5b319a8c1ec7d07fb2871a36ef7a
Koichi <koichi89592@gmail.com>
2021-03-16 18:15:07 +0900

    Add button property

--- workspace/react-typescript/rts/src/props/Child.tsx
+++ workspace/react-typescript/rts/src/props/Child.tsx
@@ -1,7 +1,22 @@
 interface ChildProps {
   color: string;
+  onClick: () => void;
 }
 
-export const Child = ({color}: ChildProps) => {
-  return <div>{color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>CLick me</button>
+    </div>
+  );
+};
+
+export const ChildAsFc: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      {color}
+      <button onClick={onClick}>CLick me</button>
+    </div>
+  );
 };
