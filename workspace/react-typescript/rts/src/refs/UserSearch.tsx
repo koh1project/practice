2d48a1068a3e62d458976bfef9ab4f2aac6c9e5d
Koichi <koichi89592@gmail.com>
2021-03-18 18:37:05 +0900

    Use ref function to focus

--- workspace/react-typescript/rts/src/refs/UserSearch.tsx
+++ workspace/react-typescript/rts/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 const users = [
   { name: 'Sarah', age: 20 },
@@ -10,6 +10,10 @@ const UserSearch: React.FC = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    inputRef.current && inputRef.current.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => user.name === name);
